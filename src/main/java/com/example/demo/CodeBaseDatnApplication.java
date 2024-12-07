cc86dc52363c0bda8911a91463b5a126a241ba73
Minh200463 <kuidndjj@gmail.com>
2024-12-07 20:33:55 +0700

    mmm

--- src/main/java/com/example/demo/CodeBaseDatnApplication.java
+++ src/main/java/com/example/demo/CodeBaseDatnApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CodeBaseDatnApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CodeBaseDatnApplication.class, args);
 	}
